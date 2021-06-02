260ac44f95983ae92fee6bfef667bbe177928978
villivald <maxim.villivald@gmail.com>
2021-06-02 15:19:30 +0300

    add cra & villivald sites

---
+++ _data/sites/create-react-app.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "create-react-app.com", // optional, falls back to object key
+	description: "create-react-app.com",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://create-react-app.com"
+	]
+};
--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,10 +1,10 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
-	description: "The default sites that get tested",
+	name: "villivald.com", // optional, falls back to object key
+	description: "villivald.com",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://www.speedlify.dev/"
+		"https://villivald.com"
 	]
 };
