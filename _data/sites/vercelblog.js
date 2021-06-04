6604f74fd10adbc4598bf7338cc2291196f3e777
villivald <maxim.villivald@gmail.com>
2021-06-04 16:35:32 +0300

    add vercel blog

---
+++ _data/sites/vercelblog.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "Blog on Vercel", // optional, falls back to object key
+	description: "Blog on Vercel",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://create-react-app-blog.vercel.app/"
+	]
+};
